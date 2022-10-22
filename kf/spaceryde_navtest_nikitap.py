07f76eb2bd3d800506e3c85d5ef50876a5c3c3ce
Nikita <npersikov1@gmail.com>
2022-10-22 00:06:29 -0400

    animation works for two thirds of first section

--- kf/spaceryde_navtest_nikitap.py
+++ kf/spaceryde_navtest_nikitap.py
@@ -6,6 +6,8 @@ import matplotlib.pyplot as plt
 import numpy as np
 import math
 from numpy import random
+import matplotlib.patches as patches
+import matplotlib.animation as animation
 
 # Initial states of relevant objects. Column vectors.
 robot_state_bodyFromOrigin_BodyFrame = np.array([[0], [0], [0], [0], [0], [np.pi/2], [0], [0], [0], [0], [0], [0]]);
@@ -22,10 +24,6 @@ v_max_mps       = 5
 alpha_radps2    = 2
 omega_max_radps = 1
 
-# fig = plt.figure()
-# ax = plt.axes(projection='3d')
-# ax.contour3D(x, y, z, 50, cmap='binary')
-
 # Plot util vars
 window_size = 10
 
@@ -34,48 +32,74 @@ time_step = 1.0/25.0 # 25 Hz frequency
 time_index = 0
 
 # plot the initial scene
+fig = plt.figure()
 ax = plt.axes()
-ax.set_xlim(-window_size,window_size)
-ax.set_ylim(-window_size,window_size)
+ax.set_xlim(-2,25)
+ax.set_ylim(-2,7)
+ax.set_aspect('equal', adjustable='box')
 beacon = plt.Circle((0, 5), 0.1, fc='blue',ec="red")
-robot = plt.Rectangle((-length/2, -width/2), length, width, fc='blue',ec="red")
-plt.gca().add_patch(robot)
-plt.gca().add_patch(beacon)
-plt.axis('scaled')
-fig = plt.figure("animation")
-im = plt.imshow(robot_state_bodyFromOrigin_BodyFrame[0:3])
+robot = patches.Rectangle((0, 0), 0, 0, fc='y')
 
-# Test Scenario Simulation
+# ======================== Test Scenario Simulation ===========================
+
 # Step 1: accelerate until robot reaches its max speed in the body x axis
 # NOTE: this can be integrated as an x_dot = Ax + Bu system, but A is a sparse
 # matrix, so computational power is saved by doing the simulation this way.
-state_history = np.array([[0], [0], [0]])
-time_history = np.array([0])
-current_time_s = 0.0;
-while np.linalg.norm(robot_state_bodyFromOrigin_BodyFrame[6:9]) < v_max_mps : # while the norm of the translational velocity is below the max
+state_history   = robot_state_bodyFromOrigin_BodyFrame
+time_history    = np.array([0.0])
+current_time_s  = 0.0;
+
+# while the norm of the translational velocity is below the max
+while np.linalg.norm(robot_state_bodyFromOrigin_BodyFrame[6:9]) < v_max_mps :
 
     # Integrate the equations of motion
     robot_state_bodyFromOrigin_BodyFrame[0:3] = robot_state_bodyFromOrigin_BodyFrame[0:3] + np.multiply(robot_state_bodyFromOrigin_BodyFrame[6:9], time_step)
     robot_state_bodyFromOrigin_BodyFrame[6:9] = robot_state_bodyFromOrigin_BodyFrame[6:9] + np.multiply(np.array([[a_mps2], [0], [0]]), time_step)
 
+    # If the simulation reached a point where the velocity is greater than the
+    # maximum possible value, limit the velocity to the max value but keep the
+    # same direction vector
+    if np.linalg.norm(robot_state_bodyFromOrigin_BodyFrame[6:9]) > v_max_mps :
+        robot_state_bodyFromOrigin_BodyFrame[6:9] = robot_state_bodyFromOrigin_BodyFrame[6:9] / np.linalg.norm(robot_state_bodyFromOrigin_BodyFrame[6:9]) * v_max_mps
+
     # Save and update time
     current_time_s = current_time_s + time_step
-
+    time_history = np.append(time_history, current_time_s)
 
     # Add state vector at time step to a variable for the animation
-    state_history = np.c_[state_history, robot_state_bodyFromOrigin_BodyFrame[0:3]]
+    state_history = np.c_[state_history, robot_state_bodyFromOrigin_BodyFrame]
 
-    print(np.linalg.norm(robot_state_bodyFromOrigin_BodyFrame[6:9]))
+    # print(np.linalg.norm(robot_state_bodyFromOrigin_BodyFrame[6:9]))
+    # print(robot_state_bodyFromOrigin_BodyFrame[5,0])
+
+
+
+additional_time_s = 5
+time_at_max_speed = current_time_s
+for t in range(int(additional_time_s/time_step)) :
+
+    # Integrate the equation of motion
+    robot_state_bodyFromOrigin_BodyFrame[0:3] = robot_state_bodyFromOrigin_BodyFrame[0:3] + np.multiply(robot_state_bodyFromOrigin_BodyFrame[6:9], time_step)
+
+    # Save and update time
+    current_time_s = current_time_s + time_step
+    time_history = np.append(time_history, current_time_s)
+
+    # Add state vector at time step to a variable for the animation
+    state_history = np.c_[state_history, robot_state_bodyFromOrigin_BodyFrame]
 
 def init():
     ax.add_patch(robot)
     return robot,
+
 def animate(i):
-    patch = patches.Rectangle((state_history[0,i], state_history[1,i]), 1.2, 1.0, fc ='y',angle = -np.rad2deg(yaw[i]))
-    return patch,
-anim = animation.FuncAnimation(fig, animate,
-                               init_func=init,
-                               frames=360,
-                               interval=1,
-                               blit=True)
+    robot.set_width(width)
+    robot.set_height(length)
+    robot.set_xy([state_history[0,i], state_history[1,i]])
+    robot.angle = np.rad2deg(state_history[5, i]) # angle rotated about z axis
+    print('speed: ' + str(np.linalg.norm(state_history[6:9, i])))
+    print('time: ' + str(time_history[i]))
+    return robot,
+
+anim = animation.FuncAnimation(fig, animate, init_func = init, frames = len(state_history[0]), interval = 500, blit = True, repeat = False)
 plt.show()
