762d7a8a1cdb9c7186de88368789f08d77acdad6
Nikita <npersikov1@gmail.com>
2022-11-16 19:06:47 -0500

    KF: cleaned errors, debugging in progress. Getting an interesting result which is ultimately wrong.

--- kf/od_kf_practice.py
+++ kf/od_kf_practice.py
@@ -4,6 +4,7 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import math
+import time
 from numpy import random
 from numpy import linalg
 
@@ -15,7 +16,7 @@ x = np.array([[9000], [0], [0]])
 v = np.array([[0], [5], [0]])
 
 # This function integrates the orbit and plots it
-def orbit_determination(x, v, period, dt) :
+def orbit_determination(x, v, period, dt, mu) :
     # The list of real state vectors
     x_list = x
     v_list = v
@@ -28,8 +29,11 @@ def orbit_determination(x, v, period, dt) :
     x_nav_list = np.add(x, random.normal(0, sigma_x, size=(3,1)))
     v_nav_list = np.add(v, random.normal(0, sigma_v, size=(3,1)))
 
+    # Initialize the list of determined states
+    x_vec_det_list = np.vstack((x, v))
+
     # =========== Initial Values for State Estimation ===========
-    x_0 = np.vstack(x, v)
+    x_0 = np.vstack((x, v))
     P_0 = np.diag([sigma_x**2, sigma_x**2, sigma_x**2, sigma_v**2, sigma_v**2, sigma_v**2])
     H   = get_H(x_0)
     Q   = np.zeros((len(x_0), len(x_0)))
@@ -47,25 +51,33 @@ def orbit_determination(x, v, period, dt) :
     x_vec = x_0
     x_vec_u = x_0
     P_u = P_0
-    for time in times :
+    index = 1   # Start at 1 since the first update uses the first measurement
+    for t in times :
+        run = index + 1
+        # print "Run: ", run
+        # print "Progress: ", t/period*100, "%"
 
-        # ============ State Estimation ===============
+        # ============ State Estimation Prediction ===============
 
         # Predict
-        stm = get_stm()
-        x_p = predict_next_state(x_vec_u, stm)
-        P_p = predict_next_covariance(P_u, stm, Q)
+        ddxdx        = get_stm_jacobian(x_vec_u[0:3], mu)
+        # NOTE: It would be proper to calculate the stm rate by multiplying
+        # ddxdx by the original phi matrix, but since it starts as an identity
+        # matrix and is only integrated for one time step, this step does
+        # nothing overall.
+        dphi        = np.matmul(ddxdx, np.identity(6))
+        stm         = np.identity(6) + dphi*dt
+        x_vec_p     = predict_next_state(x_vec_u, stm)
+        P_p         = predict_next_covariance(P_u, stm, Q)
+        meas_p      = get_meas_from_state(x_vec_p)
 
-        # Update
-
-
-        # =============================================
+        # =======================================================
 
         # Incremental orbit physics integration
-        ddr = -mu*x/np.linalg.norm(x)**3
-        v   = v + ddr*dt
-        x   = x + v*dt
-        x_vec = np.vstack(x, v)
+        ddr     = -mu*x/np.linalg.norm(x)**3
+        v       = v + ddr*dt
+        x       = x + v*dt
+        x_vec   = np.vstack((x, v))
 
         # Save real state
         x_list = np.c_[x_list, x]
@@ -75,14 +87,40 @@ def orbit_determination(x, v, period, dt) :
         x_nav_list = np.c_[x_nav_list, np.add(x, random.normal(0, sigma_x, size=(3,1)))]
         v_nav_list = np.c_[v_nav_list, np.add(v, random.normal(0, sigma_v, size=(3,1)))]
 
+        # ============ State Estimation Update ==================
+
+        # Update
+        H           = get_H(x_vec_p)
+        K           = get_kalman_gain(P_p, H, R)
+        meas_state  = np.vstack((x_nav_list[:, [index]], v_nav_list[:, [index]]))
+
+        # print "predicted measurment: ", meas_p
+        # print "real measurement: ", get_meas_from_state(meas_state)
+        # print "predicted state vector: ", x_vec_p
+        # print "K: ", K
+        x_vec_u     = x_vec_p + np.matmul(K, get_meas_from_state(meas_state) - meas_p)
+        P_u         = P_p - np.matmul(K, np.matmul(H, P_p))
+        # time.sleep(10)
+
+        # =======================================================
+
+        # Save estimated state
+        x_vec_det_list = np.c_[x_vec_det_list, x_vec_u]
+
+        # Increment utility index
+        index = index + 1
+
     # Plot the period
+    plt.figure(1)
     plt.plot(x_nav_list[0,:], x_nav_list[1,:])
+    plt.figure(2)
+    plt.plot(x_vec_det_list[0,:], x_vec_det_list[1,:])
     plt.show()
 
 def plot_orbit(x, v, period, dt) :
     x_list = x
     times = np.arange(0, period, dt)
-    for time in times :
+    for t in times :
         ddr     = -mu*x/np.linalg.norm(x)**3
         v       = v + ddr*dt
         x       = x + v*dt
@@ -116,8 +154,8 @@ def get_period(x, v, mu) :
 #   R:      The measurment noise covariance matrix
 def get_kalman_gain(P_p, H, R):
     term_1  = np.matmul(P_p, np.transpose(H))
-    term_2  = np.matmul(np.matmul(H, P_p), np.transpose(H))
-    term_3  = np.inv(np.add(term_2, R))
+    term_2  = np.matmul(H, np.matmul(P_p, np.transpose(H)))
+    term_3  = np.linalg.inv(np.add(term_2, R))
     K       = np.matmul(term_1, term_3)
     return K
 
@@ -135,11 +173,12 @@ def update_state_estimate(x_p, K, y, z_p):
 
 # Predict the next state's covariance matrix using old one and process noise Q.
 def predict_next_covariance(P_u_old, stm, Q):
-    P_p = np.add(np.matmul(np.matmul(stm, P_u_old), np.transpose(stm)), Q)
+    P_p = np.add(np.matmul(stm, np.matmul(P_u_old, np.transpose(stm))), Q)
+    return P_p
 
 # Update the predicted covariance matrix.
 def update_covariance(K, H, P_p):
-    mat_prod = np.matmul(np.matmul(K, H), P_p)
+    mat_prod = np.matmul(K, np.matmul(H, P_p))
     P_u = np.add(P_p, mat_prod)
     return P_u
 
@@ -155,7 +194,8 @@ def update_covariance(K, H, P_p):
 # components.
 def get_H(x):
     # TODO figure out how to split up this line nicely.
-    H = np.array([[0,0,0,1,0,0], [0,0,0,0,1,0], [0,0,0,0,0,1], [2*x(1), 2*x(2), 2*x(3), 0, 0, 0], [0, 0, 0, 2*x(4), 2*x(5), 2*x(6)]])
+    H = np.array([[0,0,0,1,0,0], [0,0,0,0,1,0], [0,0,0,0,0,1], [2*x[0,0], 2*x[1,0], 2*x[2,0], 0, 0, 0], [0, 0, 0, 2*x[3,0], 2*x[4,0], 2*x[5,0]]])
+    return H
 
 # Get the matrix needed to integrate the state transition matrix from one time
 # to the next in an orbit.
@@ -168,7 +208,7 @@ def get_H(x):
 # stm.
 def get_stm_jacobian(r, mu):
 
-    zeros   = np.zeros(len(r), len(r))
+    zeros   = np.zeros((len(r), len(r)))
     eye     = np.identity(len(r))
     partial = np.identity(len(r))
     rows    = len(r)
@@ -180,16 +220,22 @@ def get_stm_jacobian(r, mu):
     for row in range(rows) :
         for col in range(cols) :
             if row == col :
-                partial(row, col) = (-mu*(np.linalg.norm(r)**2 - 3*r(row)**2)) / (np.linalg.norm(r)**5)
+                partial[row, col] = (-mu*(np.linalg.norm(r)**2 - 3*r[row]**2)) / (np.linalg.norm(r)**5)
             else :
-                partial(row, col) = 3*mu*r(row)*r(col)/(np.linalg.norm(r)**5)
+                partial[row, col] = 3*mu*r[row]*r[col]/(np.linalg.norm(r)**5)
 
     # Concatenate the four sub-matrices to get the stm stm (yes the stm stm).
-    top     = np.hstack(zeros, eye)
-    bot     = np.hstack(partial, zeros)
-    dgdx    = np.vstack(top, bot)
+    top     = np.hstack((zeros, eye))
+    bot     = np.hstack((partial, zeros))
+    dgdx    = np.vstack((top, bot))
 
     # Finally, return the stm stm
     return dgdx
 
-orbit_determination(x, v, get_period(x, v, mu), 0.1)
+# This function is h(x), which gets the predicted measurement from the predicted
+# state and a realistic measurment from a realistic state.
+def get_meas_from_state(x_vec) :
+    h = np.array([[x_vec[0,0]], [x_vec[1,0]], [x_vec[2,0]], [np.linalg.norm(x_vec[0:3,0])], [np.linalg.norm(x_vec[3:len(x_vec)])]])
+    return h
+
+orbit_determination(x, v, get_period(x, v, mu), 1, mu)
